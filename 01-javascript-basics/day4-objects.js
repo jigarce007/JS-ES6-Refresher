878ce48c2f75f3aac0ea0b44ce8cc05e931b94dd
Jigar Makwana <jigarce007@gmail.com>
2026-02-23 21:24:32 +0530

    Object Understanding

--- 01-javascript-basics/day4-objects.js
+++ 01-javascript-basics/day4-objects.js
@@ -1,6 +1,7 @@
 //An Object stores data in key-value pairs.
 //example,
 
+
 const user = {
     name : "Jigar",
     age : 32,
@@ -20,5 +21,31 @@ console.log(`user's age is ${age}`)
 //Very important in real apps.
 const key = "name"
 console.log(user[key])
+
+//Add/U/pdate Property 
+//Note : Objects are mutable by default.
+user.isActive = true //Added
+user.company = "Boost Bank"
+console.log(user)
+
+//Delete Property
+delete user.isActive
+console.log(user)
+
+//================================
+//Object Methods (Very Important) - Built-In Object Helpers
+
+//1.Returns array of Keys of Object
+console.log(Object.keys(user))
+
+//2.Returns array of values
+console.log(Object.values(user))
+
+//3.Returns array of Key:Value pairs from object
+//Very powerful with map()
+console.log(Object.entries(user))
+
+//4.hasOwnProperty() - Used for validation.
+console.log(user.hasOwnProperty("name"))
 
 
